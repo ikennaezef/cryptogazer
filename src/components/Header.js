f3b9690d41a05b9ee584969f70bfeb0859a27a6d
Ikenna <iykee0333@gmail.com>
2022-07-27 14:52:30 +0100

    currency change logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,22 @@ import React from 'react'
 
 import {Box, Container, Flex, Heading, Select} from "@chakra-ui/react";
 
+import {useAppContext} from "../context/AppContext";
+
 const Header = () => {
+
+  const {supportedCurrencies, setCurrency } = useAppContext();
+
   return (
     <Box py={5}>
       <Container maxW='container.xl'>
         <Flex align='center' justify='space-between'>
-          <Heading color='cyan.300'>CryptoGazer</Heading>
+          <Heading fontSize='1.5rem' fontWeight='600' color='cyan.300'>CryptoGazer</Heading>
           <Box>
-            <Select>
-              <option value="USD">USD</option>
-              <option value="NGN">NGN</option>
+            <Select onChange={(e) => setCurrency(e.target.value)} colorScheme='blue.700' >
+              {supportedCurrencies.map((currency, i) => (
+                <option style={{background: '#2D3748'}} key={i} value={currency}>{currency}</option>
+              ))}
             </Select>
           </Box>
         </Flex>
